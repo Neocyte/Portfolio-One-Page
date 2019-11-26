953a0abd87009b92e74c7c870c811ba65e9263e9
Mason Wu <masonwu8462@yahoo.com>
2019-11-26 16:34:46 -0500

    Feat: Added more projects
    I also changed to a dark theme and refactored the hover function.

--- js/app.js
+++ js/app.js
@@ -1,32 +1,19 @@
 // Animates project previews on hover
-(function hoverProject() {
-  const project1 = document.querySelector('.project1');
-  const projectPreview1 = document.querySelector('.project-preview1');
+function hoverProject(project, preview, jpg, gif) {
+  const proj = document.querySelector(project);
+  const prev = document.querySelector(preview);
 
-  const project2 = document.querySelector('.project2');
-  const projectPreview2 = document.querySelector('.project-preview2');
+  proj.addEventListener('mouseover', function() {
+    prev.setAttribute('src', gif);
+  });
+  proj.addEventListener('mouseout', function() {
+    prev.setAttribute('src', jpg);
+  });
+}
 
-  const project3 = document.querySelector('.project3');
-  const projectPreview3 = document.querySelector('.project-preview3');
-
-  project1.addEventListener('mouseover', function() {
-    projectPreview1.setAttribute('src', 'images/memory game gif.gif');
-  });
-  project1.addEventListener('mouseout', function() {
-    projectPreview1.setAttribute('src', 'images/memory game static.jpg');
-  });
-
-  project2.addEventListener('mouseover', function() {
-    projectPreview2.setAttribute('src', 'images/frogger game gif.gif');
-  });
-  project2.addEventListener('mouseout', function() {
-    projectPreview2.setAttribute('src', 'images/frogger game static.jpg');
-  });
-
-  project3.addEventListener('mouseover', function() {
-    projectPreview3.setAttribute('src', 'images/feedreader gif.gif');
-  });
-  project3.addEventListener('mouseout', function() {
-    projectPreview3.setAttribute('src', 'images/feedreader static.jpg');
-  });
-})();
+hoverProject('.project1', '.project-preview1', 'images/memory game static.jpg', 'images/memory game gif.gif');
+hoverProject('.project2', '.project-preview2', 'images/frogger game static.jpg', 'images/frogger game gif.gif');
+hoverProject('.project3', '.project-preview3', 'images/feedreader static.jpg', 'images/feedreader gif.gif');
+hoverProject('.project4', '.project-preview4', 'images/restaurant static.jpg', 'images/restaurant gif.gif');
+hoverProject('.project5', '.project-preview5', 'images/myreads static.jpg', 'images/myreads gif.gif');
+hoverProject('.project6', '.project-preview6', 'images/neighborhood static.jpg', 'images/neighborhood gif.gif');
